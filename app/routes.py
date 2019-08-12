aa9aad712fd60ac89cbeb01e7afc6d3612351f3c
doctorblinch <docblinch@gmail.com>
2019-08-12 20:49:45 +0300

    Bot page history update, compare page in process

--- app/routes.py
+++ app/routes.py
@@ -20,10 +20,49 @@ def bot_page(id):
     history = scrapes_of_user(id=id)
     return render_template('bot.html', user = user, history = history, users = users)
 
+
+def compare_users_histories(history1, history2):
+    engines1 = set()
+    engines2 = set()
+    for i in history1:
+        engines1.add(i[7])
+    for i in history2:
+        engines2.add(i[7])
+
+    #print(engines1, engines2)
+    common_engines = engines1.intersection(engines2)
+
+    result = {}
+    queries1 = set()
+    queries2 = set()
+    for engine in common_engines:
+        common_querries = set()
+        different_querries = set()
+        for i in history1:
+            if i[7] == engine:
+                queries1.add(i[2])
+        for i in history2:
+            if i[7] == engine:
+                queries2.add(i[2])
+
+        common_querries = queries1.intersection(queries2)
+        different_querries = queries1.union(queries2).difference(common_querries)
+        tmp = {'common': common_querries,'different': different_querries}
+        print(engine)
+        result.update({engine : tmp})
+    return result
+
+
+
 @app.route('/compare/<id1>/<id2>')
 def compare(id1, id2):
     user1 = read_user_from_db(id=id1)
     history1 = scrapes_of_user(id=id1)
     user2 = read_user_from_db(id=id2)
     history2 = scrapes_of_user(id=id2)
-    return render_template('compare.html', user1 = user1, user2 = user2, history1 = history1, history2 = history2)
+
+
+    comparation = compare_users_histories(history1, history2)
+    print('UFFFFFFFFFFFFFFFFFF=',comparation)
+
+    return render_template('compare.html', user1 = user1, user2 = user2, history1 = history1, history2 = history2, cmp = comparation)
